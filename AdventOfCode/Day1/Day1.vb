199640d0b38c3798c7b1c0309e2b2e7df3bf2b15
Rob Herman <sitnaltax@gmail.com>
2022-12-01 13:59:12 -0500

    day 1

---
+++ AdventOfCode/Day1/Day1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+
+Public Class Day1
+    Public Shared Sub calculate()
+        Dim lines() As String = IO.File.ReadAllLines("C:\Users\Rob\source\repos\AdventOfCode\AdventOfCode\Day1\input.txt")
+        Dim runningTotal As Integer
+        Dim elves As List(Of Integer) = New List(Of Integer)
+        runningTotal = 0
+        For Each line As String In lines
+            If Len(line) < 1 Then
+                elves.Add(runningTotal)
+                runningTotal = 0
+            Else
+                runningTotal += Convert.ToInt32(line)
+            End If
+        Next
+        Console.WriteLine(elves.Max)
+        elves.Sort()
+        Console.WriteLine(elves.GetRange(elves.Count - 3, 3).Sum)
+    End Sub
+End Class
--- AdventOfCode/Program.vb
+++ AdventOfCode/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Day1.calculate()
     End Sub
 End Module
