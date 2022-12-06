efc435496a3e15b475b26af183092fff00a056e9
Rob Herman <sitnaltax@gmail.com>
2022-12-06 00:41:10 -0500

    day 6

---
+++ AdventOfCode/Day6/Day6.vb
@@ -0,0 +1,22 @@
+﻿Public Class Day6
+    Public Shared Sub calculate()
+        Dim inputs() As String = IO.File.ReadAllLines("C:\Users\Rob\source\repos\AdventOfCode\AdventOfCode\Day6\input.txt")
+        Dim Line As String = inputs(0)
+        Const SIZE = 14
+        Dim buffer(SIZE - 1) As Char
+        For i As Integer = 0 To SIZE - 1
+            buffer(i) = Line(0)
+        Next
+        For i As Integer = 0 To Line.Count - 1
+            buffer(i Mod SIZE) = Line(i)
+            Dim hash As HashSet(Of Char) = New HashSet(Of Char)
+            For Each entry As Char In buffer
+                hash.Add(entry)
+            Next
+            If hash.Count = SIZE Then
+                Console.WriteLine(i + 1)
+                Exit For
+            End If
+        Next
+    End Sub
+End Class
--- AdventOfCode/Program.vb
+++ AdventOfCode/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Day5.calculate2()
+        Day6.calculate()
     End Sub
 End Module
